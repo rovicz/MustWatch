f31ed9da476c89740656a7da17c32de121b9c7b6
Victor Fernandes <roviczzf@gmail.com>
2023-11-01 21:33:16 -0300

    trying to understand

--- src/Components/GetMovie.tsx
+++ src/Components/GetMovie.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 
 const GetMovie = () => {
+  const [movieList, setMovieList] = React.useState([]);
+
   const useFetch = () => {
     fetch(
       "https://api.themoviedb.org/3/discover/movie?api_key=64ad570da067d64c452016d749b28aca"
     )
       .then((r) => r.json())
-      .then((json) => console.log(json.results));
+      .then((json) => setMovieList(json.results));
   };
 
   React.useEffect(() => {
     useFetch();
   }, []);
 
+  console.log(movieList);
+
   return (
     <div>
-      <ul></ul>
+      <ul>
+        {movieList.map((movie) => (
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+        ))}
+      </ul>
     </div>
   );
 };
