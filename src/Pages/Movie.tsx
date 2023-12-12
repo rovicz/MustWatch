fcc70222a5be689521a9d4a1d6fd7f15b78d7d8e
Victor Fernandes <roviczzf@gmail.com>
2023-12-12 19:02:36 -0300

    MustWatch - 1.6 -new Movie.tsx style

--- src/Pages/Movie.tsx
+++ src/Pages/Movie.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import React from "react";
 import { FaStar } from "react-icons/fa";
 import { GiPayMoney, GiReceiveMoney } from "react-icons/gi";
+import imdb_logo from "../assets/imdb-logo.svg";
 import { RiTimeFill } from "react-icons/ri";
 
 type MovieInfos = {
@@ -13,11 +14,13 @@ type MovieInfos = {
   budget: number;
   revenue: number;
   runtime: number;
+  imdb_id: string;
 };
 
 const movieIMG = import.meta.env.VITE_IMG;
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
+const imdb = import.meta.env.VITE_IMDB_SEARCH;
 
 const Movie = () => {
   const { id } = useParams();
@@ -43,35 +46,44 @@ const Movie = () => {
 
   if (!movie) return null;
   return (
-    <div className="container movie-desc">
+    <div className="container">
       <div>
-        <h2 className="title movie-title">{movie.title}</h2>
-        <div className="movieinfo-box movie-info">
+        <div className=" movie-info">
           <img src={movieIMG + movie.poster_path} alt={movie.title} />
-          <p className="tagline">{movie.tagline}</p>
-          <p className="budget">
-            <GiPayMoney /> Budget:{" "}
-            {movie.budget.toLocaleString("us", {
-              style: "currency",
-              currency: "USD",
-            })}
-          </p>
-          <p className="revenue">
-            <GiReceiveMoney /> Revenue:{" "}
-            {movie.revenue.toLocaleString("us", {
-              style: "currency",
-              currency: "USD",
-            })}
-          </p>
-          <p className="runtime">
-            <RiTimeFill /> {timeCoverter(movie.runtime)}
-          </p>
-          <p className="vote-average">
-            <FaStar /> {movie.vote_average.toFixed(2)}
-          </p>
+          <div className="movie-desc">
+            <h2 className="movie-title">{movie.title}</h2>
+            <p className="tagline">{movie.tagline}</p>
+            <p className="budget">
+              <GiPayMoney /> Budget:{" "}
+              {movie.budget.toLocaleString("us", {
+                style: "currency",
+                currency: "USD",
+              })}
+            </p>
+            <p className="revenue">
+              <GiReceiveMoney /> Revenue:{" "}
+              {movie.revenue.toLocaleString("us", {
+                style: "currency",
+                currency: "USD",
+              })}
+            </p>
+            <p className="runtime">
+              <RiTimeFill /> {timeCoverter(movie.runtime)}
+            </p>
+            <p className="vote-average">
+              <FaStar /> {movie.vote_average.toFixed(2)}
+            </p>
 
-          <p className="overview-title">Overview:</p>
-          <p className="overview"> {movie.overview}</p>
+            <p className="overview-title">Overview:</p>
+            <p className="overview"> {movie.overview}</p>
+
+            <div className="imdb-button">
+              <p>Get the experts' opinions:</p>
+              <a target="blank" href={imdb + movie.imdb_id}>
+                <img src={imdb_logo} alt="" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
