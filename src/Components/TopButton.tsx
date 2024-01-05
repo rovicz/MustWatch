3d8137ed853a3354af87a3c8711678dafc6b7a81
Victor Fernandes <roviczzf@gmail.com>
2024-01-05 01:39:52 -0300

    MustWatch 1.8.2 - Fixing code and improving style

--- src/Components/TopButton.tsx
+++ src/Components/TopButton.tsx
@@ -1,5 +1,33 @@
 import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
-import initScrollTop from "./TopButtonFunction";
+
+function initScrollTop() {
+  const button = document.getElementById("topButton") as HTMLButtonElement;
+  const buttonSVG = document.querySelector("#topButton svg") as SVGAElement;
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      button.style.display = "block";
+      button.style.animation = "dropDown 0.3s";
+      buttonSVG.style.animation = "dropDown 0.3s";
+    } else {
+      button.style.display = "none";
+    }
+  }
+
+  function topScroll() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  button.addEventListener("click", topScroll);
+}
 
 const TopButton = () => {
   window.onload = function () {
